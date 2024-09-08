4f2cf0f15c6910949975451db9f02649258cc8a9
Anastasia18432 <zastupanastasia7@gmail.com>
2024-09-08 19:30:18 +0300

    add

--- my-app/src/components/employers-add-form/employers-add-form.js
+++ my-app/src/components/employers-add-form/employers-add-form.js
@@ -1,27 +1,51 @@
+import { Component } from "react";
 import "./employers-add-form.css";
 
-const EmployersAddFrom = () => {
-  return (
-    <div className="app-add-form">
-      <h3>Add a new employee</h3>
-      <form className="add-form d-flex">
-        <input
-          type="text"
-          className="form-control new-post-label"
-          placeholder="What is his name?"
-        />
-        <input
-          type="number"
-          className="form-control new-post-label"
-          placeholder="salary in dollars?"
-        />
+class EmployersAddFrom extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      salary: "",
+    };
+  }
 
-        <button type="submit" className="btn btn-outline-light">
-          Add
-        </button>
-      </form>
-    </div>
-  );
-};
+  onValueChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  render() {
+    const { name, salary } = this.state;
+    return (
+      <div className="app-add-form">
+        <h3>Add a new employee</h3>
+        <form className="add-form d-flex">
+          <input
+            type="text"
+            className="form-control new-post-label"
+            placeholder="What is his name?"
+            name="name"
+            value={name}
+            onChange={this.onValueChange}
+          />
+          <input
+            type="number"
+            className="form-control new-post-label"
+            placeholder="salary in dollars?"
+            name="salary"
+            value={salary}
+            onChange={this.onValueChange}
+          />
+
+          <button type="submit" className="btn btn-outline-light">
+            Add
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
 
 export default EmployersAddFrom;
