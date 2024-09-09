fd09bd90a6f6a0b860389609bd64c67614898866
Anastasia18432 <zastupanastasia7@gmail.com>
2024-09-09 15:05:16 +0300

    add deleted and added

--- my-app/src/components/app/app.js
+++ my-app/src/components/app/app.js
@@ -1,3 +1,5 @@
+import { Component } from "react";
+
 import AppInfo from "../app-info/app-info";
 import SearchPanel from "../search-panel/search-panel";
 import AppFilter from "../app-filter/app-filter";
@@ -6,24 +8,56 @@ import EmployersAddFrom from "../employers-add-form/employers-add-form";
 
 import "./app.css";
 
-function App() {
-  const data = [
-    { name: "Alex D.", salary: 800, increase: false, id: 1 },
-    { name: "Maria M.", salary: 500, increase: true, id: 2 },
-    { name: "Anastasia S.", salary: 1100, increase: false, id: 3 },
-  ];
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [
+        { name: "Alex D.", salary: 800, increase: false, id: 1 },
+        { name: "Maria M.", salary: 500, increase: true, id: 2 },
+        { name: "Anastasia S.", salary: 1100, increase: false, id: 3 },
+      ],
+    };
+    this.maxId = 4;
+  }
 
-  return (
-    <div className="app">
-      <AppInfo />
-      <div className="search-panel">
-        <SearchPanel />
-        <AppFilter />
+  deleteItem = (id) => {
+    this.setState(({ data }) => {
+      return {
+        data: data.filter((item) => item.id !== id),
+      };
+    });
+  };
+
+  addItem = (name, salary) => {
+    const newItem = {
+      name,
+      salary,
+      increase: false,
+      id: this.maxId++,
+    };
+    this.setState(({ data }) => {
+      const newArray = [...data, newItem];
+
+      return {
+        data: newArray,
+      };
+    });
+  };
+
+  render() {
+    return (
+      <div className="app">
+        <AppInfo />
+        <div className="search-panel">
+          <SearchPanel />
+          <AppFilter />
+        </div>
+        <EmployersList data={this.state.data} onDelete={this.deleteItem} />
+        <EmployersAddFrom onAdd={this.addItem} />
       </div>
-      <EmployersList data={data} />
-      <EmployersAddFrom />
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
